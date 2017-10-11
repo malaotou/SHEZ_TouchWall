9e21b9b51fe3266777cbb6ab94b590ec715a8a6a
malaotou <marrator@163.com>
2017-10-11 10:37:24 +0800

    Add project files.

---
+++ SHEZ_TouchWall/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using DevExpress.Xpf.Core;
+
+namespace SHEZ_TouchWall
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DevExpress.Xpf.Core.ApplicationThemeHelper.UpdateApplicationThemeName();
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            DevExpress.Xpf.Core.ApplicationThemeHelper.SaveApplicationThemeName();
+        }
+    }
+}
