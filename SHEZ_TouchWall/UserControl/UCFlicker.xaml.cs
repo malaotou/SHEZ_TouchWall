531427627fcf42dcb9194d3eedbd59099db43a50
marrator@163.com <marrator@163.com>
2017-10-22 02:19:12 +0800

    flicker 增加鼠标点击

--- SHEZ_TouchWall/UserControl/UCFlicker.xaml.cs
+++ SHEZ_TouchWall/UserControl/UCFlicker.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -30,10 +31,28 @@ namespace SHEZ_TouchWall
             Image image = new Image();
             var source = (sender as Image);
             image.Source = source.Source;
-            image.SetValue(Canvas.TopProperty, source.GetValue(Canvas.TopProperty));
-            image.SetValue(Canvas.LeftProperty, source.GetValue(Canvas.LeftProperty));
+            image.SetValue(Canvas.TopProperty, GetMousePosition().Y);
+            image.SetValue(Canvas.LeftProperty, GetMousePosition().X);
             image.IsManipulationEnabled = true;
             ((SHEZ_TouchWall.MainWindow)Application.Current.MainWindow).container.Children.Add(image);
+        }
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
         }
     }
 }
